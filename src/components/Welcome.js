be0511a85795cb6aabe39d32c30da3c9e7313d9c
Paul Maxson <paul.f.maxson@gmail.com>
2018-12-28 13:13:24 -0500

    Add Welcome mockup

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,15 @@
-// import React from 'react';
+import React from 'react'
 
-const Welcome = () => null
+const Welcome = () => (
+  <>
+    <h1>Welcome</h1>
+    <p>
+      Paul Maxson is a web design and web development student. His current
+      projects include learning to build intuitive user interfaces and improving
+      his functional programming skills.
+    </p>
+    <p>This website hosts his portfolio.</p>
+  </>
+)
 
 export default Welcome
