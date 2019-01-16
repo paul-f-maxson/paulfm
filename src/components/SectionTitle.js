b055c621cc414c1adcddd451af169466fc6b8011
Paul Maxson <paul.f.maxson@gmail.com>
2019-01-15 22:43:05 -0500

    Fix section title font-size

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -1,11 +1,11 @@
 import styled from 'styled-components'
 
 const SectionTitle = styled.h2`
+  font-size: 2rem;
   text-transform: capitalize;
   font-weight: 500;
   text-align: left;
   margin: 0 0 3rem 0;
-  margin: '0 0 3rem 0';
   line-height: 1;
   span {
     margin: 0 0 0 0.25em;
