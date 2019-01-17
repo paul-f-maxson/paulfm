01ca2c55f8b265152424ce61f83fd4f52a1a7ce9
Paul Maxson <paul.f.maxson@gmail.com>
2019-01-17 11:56:21 -0500

    Icon links

--- src/templates/index.js
+++ src/templates/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { graphql } from 'gatsby'
 import styled from 'styled-components'
+import { IconContext } from 'react-icons'
+import * as faIcons from 'react-icons/fa'
 import Helmet from 'react-helmet'
 import {
   Layout,
@@ -17,13 +19,46 @@ import config from '../utils/siteConfig'
 const WelcomeBody = styled.p`
   margin-top: 0.5em;
   line-height: 1.5em;
+  strong {
+    font-weight: 600;
+  }
 `
 
 const Index = ({ data, pageContext }) => {
   const pieces = data.allContentfulPortfolioPiece.edges
-  const {fullName, shortBio, iconLinks} = data.authorInfo
+  const { shortBio, iconLinks } = data.authorInfo
   const { currentPage } = pageContext
   const isFirstPage = currentPage === 1
+
+  const externalLinks = (
+    <>
+      <SectionTitle>Other Profiles</SectionTitle>
+      {iconLinks.map(({ name, iconName, linkUrl }) => {
+        // HACK: Convert fa icon class names (like "free-code-camp") to react-icon library keys (like "FaFreeCodeCamp"). This may break if react-icons changes their name conversion pattern, and this whole section will probably need to be rewritten if a different icon library is implemented
+        const camelIconName =
+          'Fa' +
+          iconName
+            .replace(/-([a-z])/g, match => match[1].toUpperCase())
+            .replace(/^[a-z]/, match => match[0].toUpperCase())
+
+        const Icon = faIcons[camelIconName]
+
+        return (
+          <Fragment key={name}>
+            <a href={linkUrl}>
+              <IconContext.Provider value={{size: "4em", color: "black"}}>
+              <div>
+                {/* NOTE: This will crash the build with ~"cannot render undefined" if the name conversion above fails */}
+                {/* TODO: Implement an error boundary */}
+                <Icon title={name} />
+              </div>
+              </IconContext.Provider>
+            </a>
+          </Fragment>
+        )
+      })}
+    </>
+  )
 
   const cards = (
     <CardList>
@@ -40,34 +75,49 @@ const Index = ({ data, pageContext }) => {
     </CardList>
   )
 
-  return <Layout>
+  return (
+    <Layout>
       <SEO />
-      {isFirstPage ? <>
+      {isFirstPage ? (
+        <>
           <Container>
             <PageTitle>Welcome</PageTitle>
-            <WelcomeBody>{shortBio}</WelcomeBody>
+            <WelcomeBody
+              dangerouslySetInnerHTML={{
+                __html: shortBio.childMarkdownRemark.html,
+              }}
+            />
             <WelcomeBody>This website hosts his portfolio.</WelcomeBody>
+            {externalLinks}
           </Container>
           <Container>
             <SectionTitle>Portfolio Index</SectionTitle>
             {cards}
           </Container>
-        </> : <>
+        </>
+      ) : (
+        <>
           <Helmet>
             <title>{`${config.siteTitle} - Page ${currentPage}`}</title>
           </Helmet>
           <Container>{cards}</Container>
-        </>}
+        </>
+      )}
       <Pagination context={pageContext} />
     </Layout>
+  )
 }
 
 export const query = graphql`
   query($authorContentfulProfileId: String!, $skip: Int!, $limit: Int!) {
     authorInfo: contentfulProfile(id: { eq: $authorContentfulProfileId }) {
-      fullName
-      shortBio
-      # TODO: metaDescription
+      # OPTIMIZE: fullName
+      shortBio {
+        childMarkdownRemark {
+          html
+        }
+      }
+      # OPTIMIZE: metaDescription
       iconLinks {
         name
         iconName
@@ -75,7 +125,11 @@ export const query = graphql`
       }
     }
 
-    allContentfulPortfolioPiece(sort: { fields: [publicationDate], order: DESC }, limit: $limit, skip: $skip) {
+    allContentfulPortfolioPiece(
+      sort: { fields: [publicationDate], order: DESC }
+      limit: $limit
+      skip: $skip
+    ) {
       edges {
         node {
           title
