c14b1cc98ca08e4af4cfd3dd891ab0fc3040bef5
Paul Maxson <paul.f.maxson@gmail.com>
2019-01-16 23:01:01 -0500

    Completes main layout and design

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -3,9 +3,9 @@ import styled from 'styled-components'
 const SectionTitle = styled.h2`
   font-size: 2rem;
   text-transform: capitalize;
-  font-weight: 500;
+  font-weight: 600;
   text-align: left;
-  margin: 0 0 3rem 0;
+  margin: 0 0 1.2rem 0;
   line-height: 1;
   span {
     margin: 0 0 0 0.25em;
--- src/templates/about.js
+++ src/templates/about.js
@@ -3,7 +3,14 @@ import { graphql } from 'gatsby'
 import Img from 'gatsby-image'
 import Helmet from 'react-helmet'
 import config from '../utils/siteConfig'
-import { Layout, Container, PageTitle, SectionTitle, PageBody, SEO } from '../components'
+import {
+  Layout,
+  Container,
+  PageTitle,
+  SectionTitle,
+  PageBody,
+  // SEO,
+} from '../components'
 
 // TODO: Implement SEO
 
@@ -14,7 +21,8 @@ const AboutTemplate = ({ data }) => {
     bio,
   } = data.contentfulProfile
 
-  return <Layout>
+  return (
+    <Layout>
       <Helmet>
         <title>{`About - ${config.siteTitle}`}</title>
       </Helmet>
@@ -24,17 +32,23 @@ const AboutTemplate = ({ data }) => {
         <PageTitle>About</PageTitle>
         {/* OPTIMIZE: Abstract to styled component */}
         <div style={{ maxWidth: 300, margin: '0 auto' }}>
-          <Img fluid={profilePicture.fluid} backgroundColor={'#eeeeee'} alt="Photo Credit" />
-          <p>
+          <Img
+            fluid={profilePicture.fluid}
+            backgroundColor={'#eeeeee'}
+            alt={`${profilePicture.description}`}
+            id="profile"
+          />
+          <label htmlFor="profile" style={{ "line-height": "2rem" }}>
             <em>Photo Credit &mdash; {profilePictureAttribution}</em>
-          </p>
+          </label>
         </div>
       </Container>
       <Container>
-       <SectionTitle>Bio</SectionTitle>
-        <div dangerouslySetInnerHTML={ {__html: bio.childMarkdownRemark.html}} />
+        <SectionTitle>Bio</SectionTitle>
+        <PageBody html={bio.childMarkdownRemark.html} />
       </Container>
     </Layout>
+  )
 }
 
 export const query = graphql`
@@ -45,6 +59,7 @@ export const query = graphql`
         fluid(maxWidth: 300) {
           ...GatsbyContentfulFluid_withWebp_noBase64
         }
+        description
       }
       profilePictureAttribution
       bio {
