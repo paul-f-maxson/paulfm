d054bf6e192d38881d5e041f5925668af1e9e977
Paul Maxson <paul.f.maxson@gmail.com>
2019-01-29 22:01:17 -0500

    Clean up

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -11,72 +11,68 @@ const SEO = ({
   imageHeight,
   additionalSchemaOrgJSONLD,
   additionalMetaTags,
-}) => {
-  const schemaOrgJSONLD = [
-    // Default Website Schema
-    {
-      '@context': 'http://schema.org',
-      '@type': 'WebSite',
-      url: siteConfig.siteUrl,
-      name: siteConfig.siteTitle,
-      alternateName: siteConfig.siteTitleAlt ? siteConfig.siteTitleAlt : '',
-    },
-    ...(additionalSchemaOrgJSONLD || []),
-  ]
-  return (
-    <Helmet>
-      {/* General tags */}
-      <meta
-        name="image"
-        content={imageURL || siteConfig.siteUrl + siteConfig.shareImage}
-      />
-      <meta
-        name="description"
-        content={description || siteConfig.siteDescription}
-      />
+}) => (
+  <Helmet>
+    {/* General tags */}
+    <meta
+      name="image"
+      content={imageURL || siteConfig.siteUrl + siteConfig.shareImage}
+    />
+    <meta
+      name="description"
+      content={description || siteConfig.siteDescription}
+    />
 
-      {/* Schema.org tags */}
-      <script type="application/ld+json">
-        {JSON.stringify(schemaOrgJSONLD)}
-      </script>
+    {/* Schema.org tags */}
+    <script type="application/ld+json">
+      {JSON.stringify([
+        // Default Website Schema
+        {
+          '@context': 'http://schema.org',
+          '@type': 'WebSite',
+          url: siteConfig.siteUrl,
+          name: siteConfig.siteTitle,
+          alternateName: siteConfig.siteTitleAlt ? siteConfig.siteTitleAlt : '',
+        },
+        ...(additionalSchemaOrgJSONLD || []),
+      ])}
+    </script>
 
-      {/* OpenGraph tags */}
-      <meta property="og:title" content={title || siteConfig.siteTitle} />
-      
+    {/* OpenGraph tags */}
+    <meta property="og:title" content={title || siteConfig.siteTitle} />
 
-      <meta property="og:url" content={pageUrl || siteConfig.siteUrl} />
-      <meta
-        property="og:image"
-        content={imageURL || siteConfig.siteUrl + siteConfig.shareImage}
-      />
-      <meta
-        property="og:image:width"
-        content={imageWidth || siteConfig.shareImageWidth}
-      />
-      <meta
-        property="og:image:height"
-        content={imageHeight || siteConfig.shareImageHeight}
-      />
-      <meta
-        property="og:description"
-        content={description || siteConfig.siteDescription}
-      />
+    <meta property="og:url" content={pageUrl || siteConfig.siteUrl} />
+    <meta
+      property="og:image"
+      content={imageURL || siteConfig.siteUrl + siteConfig.shareImage}
+    />
+    <meta
+      property="og:image:width"
+      content={imageWidth || siteConfig.shareImageWidth}
+    />
+    <meta
+      property="og:image:height"
+      content={imageHeight || siteConfig.shareImageHeight}
+    />
+    <meta
+      property="og:description"
+      content={description || siteConfig.siteDescription}
+    />
 
-      {/* Twitter Card tags */}
-      <meta name="twitter:card" content="summary_large_image" />
-      {siteConfig.userTwitter ? (
-        <meta name="twitter:creator" content={siteConfig.userTwitter} />
-      ) : null}
-      <meta name="twitter:title" content={title || siteConfig.siteTitle} />
-      <meta
-        name="twitter:image"
-        content={imageURL || siteConfig.siteUrl + siteConfig.shareImage}
-      />
-      <meta name="twitter:description" content={description} />
+    {/* Twitter Card tags */}
+    <meta name="twitter:card" content="summary_large_image" />
+    {siteConfig.userTwitter ? (
+      <meta name="twitter:creator" content={siteConfig.userTwitter} />
+    ) : null}
+    <meta name="twitter:title" content={title || siteConfig.siteTitle} />
+    <meta
+      name="twitter:image"
+      content={imageURL || siteConfig.siteUrl + siteConfig.shareImage}
+    />
+    <meta name="twitter:description" content={description} />
 
-      {additionalMetaTags}
-    </Helmet>
-  )
-}
+    {additionalMetaTags}
+  </Helmet>
+)
 
 export default SEO
